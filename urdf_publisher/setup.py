377bfe5003c5802b6ae629d5134ae6960bfc820c
rotematari <rotem.atri@gmail.com>
2023-12-20 11:55:54 +0200

    first commit

---
+++ urdf_publisher/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+package_name = 'urdf_publisher'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        (os.path.join('share', package_name), glob('urdf/*')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='robotics20',
+    maintainer_email='rotem.atri@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
